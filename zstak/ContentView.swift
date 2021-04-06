acdca0b46c53cdc821496eec383d9cc42c906769
Naeem <naeem3d@gmail.com>
2021-04-06 11:39:26 -0400

    egg app rewrite from swift UIKit by SwiftUI with different Timer counter and different instruction

--- zstak/ContentView.swift
+++ zstak/ContentView.swift
@@ -8,10 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var isActive = true
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var weather = "Soft"
+    @State var timepoiling = 0
+   
+    let items:[String]=["Hard","Medium","Soft"]
+    let eggTimes = ["Soft":300,"Medium":420,"Hard":720]
+   
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      
+        VStack (spacing: 50){
+            
+            Spacer()
+            Text(  "\(weather) "   + " \nTime remaining is "  )
+                .multilineTextAlignment(.center)
+                .font(.largeTitle)
+                .frame(width: 300, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .background(Capsule().fill(Color.orange).opacity(0.75))
+                .onReceive(timer) { time in
+                   
+                    
+                    if self.timepoiling > 0 {
+                        self.timepoiling -= 1
+                    }
+                }
+            Text("\(timepoiling)" + "\nSeconds   ")
+                .multilineTextAlignment(.center)
+                .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 10)
+                .background(
+                        Capsule()
+                            .fill(Color.blue)
+                            .opacity(0.85)
+                    )
+                
+            
+         
+          
+            HStack(spacing: 10){
+               
+                ForEach(items,id:\.self){ item in
+                    
+                    Button(action: {
+                        weather = item
+                        switch weather {
+                        case "Hard":
+                           
+                            timepoiling = eggTimes["Hard"]!
+                            
+                        case "Medium":
+                            timepoiling = eggTimes["Medium"]!
+                        case "Soft":
+                            timepoiling = eggTimes["Soft"]!
+                        default:
+                            timepoiling = 100
+                        }
+                        
+                        
+                    }, label: {
+                        
+                        Image("\(item)").resizable()
+                            
+                            .frame(width: 120, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                            .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                    })
+                    
+                    
+                    .onReceive(timer) { time in
+                        if Int(weather) ?? 0 > 0 {weather = String( Int(weather)! - 1)}
+                        
+                    }
+                    
+                }
+                
+          
+            }
+          
+            Spacer()
+            Spacer()
+        }
+        
+      
     }
+   
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
